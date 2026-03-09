12ee5108d5bce1347819cfcafef1c697abcb8402
Hirahi Torrejon <hirahi.torrejon101@alu.ulpgc.es>
2026-03-09 18:36:03 +0000

    Añadido dmCampaignaMain.js

---
+++ src/Pages/dmCampaignMain/dmCampaignMain.js
@@ -0,0 +1,16 @@
+import { loadTemplate } from '../../script.js';
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+
+  loadTemplate('../../Templates/headerLoggedIn.html', 'header');
+  loadTemplate('../../Templates/playersSideBar.html', 'playersSideBar');
+  loadTemplate('../../Templates/dmNote.html', 'firstNote');
+  loadTemplate('../../Templates/dmNote.html', 'secondNote');
+  loadTemplate('../../Templates/dmNote.html', 'thirdNote');
+  loadTemplate('../../Templates/dmNote.html', 'fourthNote');
+  loadTemplate('../../Templates/dmNote.html', 'fifthNote');
+  loadTemplate('../../Templates/dmNote.html', 'lastNote');
+  loadTemplate("../../Templates/logTiradas.html", 'logTirada');
+}
--- src/Pages/dmCampaignMain/dmCampaingMain.js
+++
@@ -1,9 +0,0 @@
-import { loadTemplate } from '../../script.js';
-
-document.addEventListener('DOMContentLoaded', init);
-
-function init() {
-
-  loadTemplate('../../Templates/header.html', 'header');
-  loadTemplate('../../Templates/playersSideBar.html', 'playersSideBar');
-}
