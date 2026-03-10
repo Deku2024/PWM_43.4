55c54ad8d519d3494c408839ba64908a20d900f7
ivanl <ivanlujmor@gmail.com>
2026-03-10 14:19:51 +0000

    JS cambiado para permitir todo tipo de búsquedas en el DOM

--- src/Pages/allSessions/allSessions.js
+++ src/Pages/allSessions/allSessions.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
   loadTemplate('../../Templates/headerLoggedIn.html', 'header');
-  loadTemplate('../../Templates/navBar.html', 'navBar');
-  loadTemplate('../../Templates/sessionMenu.html', 'session1');
-  loadTemplate('../../Templates/sessionMenu.html', 'session2');
-  loadTemplate('../../Templates/sessionMenu.html', 'session3');
+  loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
+  loadTemplate('../../Templates/navBar.html', 'nav');
+  loadTemplate('../../Templates/sessionMenu.html', '.session');
 }
--- src/Pages/createdSession/createdSession.js
+++ src/Pages/createdSession/createdSession.js
@@ -3,7 +3,7 @@ import { loadTemplate } from '../../script.js';
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  loadTemplate('../../Templates/userIconAndName.html', 'userIcon');
   loadTemplate('../../Templates/headerLoggedIn.html', 'header');
-  loadTemplate('../../Templates/navBar.html', 'navBar');
+  loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
+  loadTemplate('../../Templates/navBar.html', 'nav');
 }
