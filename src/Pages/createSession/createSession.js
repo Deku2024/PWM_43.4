560ef27e5a1093a622bbbddcd15dd36e8b933a6d
ivanl <ivanlujmor@gmail.com>
2026-03-13 16:04:42 +0000

    Añadida paleta de colores y empezando a refactorizar

--- src/Pages/createSession/createSession.js
+++ src/Pages/createSession/createSession.js
@@ -2,8 +2,9 @@ import { loadTemplate } from '../../script.js';
 
 document.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  loadTemplate('../../Templates/headerLoggedIn.html', 'header');
-  loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
+async function init() {
+  loadTemplate('../../Templates/headerLoggedIn.html', 'header', () => {
+    loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
+  });
   loadTemplate('../../Templates/navBar.html', 'nav');
 }
