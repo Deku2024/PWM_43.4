444bbbd786c703954162f120acd0bd6460267c79
ivanl <ivanlujmor@gmail.com>
2026-03-17 18:56:00 +0000

    allSessions refactorizado. Queda cargar por jason

--- src/Pages/createSession/createSession.js
+++ src/Pages/createSession/createSession.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
   loadTemplate('../../Templates/headerLoggedIn.html', 'header', () => {
-    loadTemplate('../../Templates/userIconAndName.html', '.userIconandName');
+    loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
   });
   loadTemplate('../../Templates/navBar.html', 'nav');
 }
