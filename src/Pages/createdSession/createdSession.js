bac2f5bb14358d6a6d529be11f4ca2b0ddb4913d
ivanl <ivanlujmor@gmail.com>
2026-03-18 17:01:34 +0000

    Pequeños cambios hechos

--- src/Pages/createdSession/createdSession.js
+++ src/Pages/createdSession/createdSession.js
@@ -3,9 +3,9 @@ import { loadTemplate } from '../../script.js';
 document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
-  loadTemplate('../../Templates/headerLoggedIn.html', 'header', () => {
+  await loadTemplate('../../Templates/headerLoggedIn.html', 'header', () => {
     loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
   });
-  loadTemplate('../../Templates/navBar.html', 'nav');
+  await loadTemplate('../../Templates/navBar.html', 'nav');
 }
 
