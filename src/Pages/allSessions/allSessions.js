b729f8d6143b832c478ece6a5b5d4d9a5eb6b1d1
ivanl <ivanlujmor@gmail.com>
2026-03-20 20:11:12 +0000

    Empezando a hacer media queries e imágenes de icónos de navegación extras añadidos

--- src/Pages/allSessions/allSessions.js
+++ src/Pages/allSessions/allSessions.js
@@ -1,10 +1,14 @@
 import { loadTemplate } from '../../script.js';
+import { initMenu } from '../../auxiliar-script.js';
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  loadTemplate('../../Templates/headerLoggedIn.html', 'header');
-  loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
+  loadTemplate('../../Templates/headerLoggedIn.html', 'header', () =>
+    loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName', () => {
+      initMenu();
+    })
+  );
   loadTemplate('../../Templates/navBar.html', 'nav');
   loadTemplate('../../Templates/sessionMenu.html', '.session');
 }
