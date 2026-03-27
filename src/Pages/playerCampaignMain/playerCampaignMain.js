5b906c09b430edddacd1dad865bf559d446c8792
Hirahi Torrejon <hirahi.torrejon101@alu.ulpgc.es>
2026-03-27 17:05:06 +0000

    Editado responsive de player campaing main (Todo: Arreglar js); Añadido logo

--- src/Pages/playerCampaignMain/playerCampaignMain.js
+++ src/Pages/playerCampaignMain/playerCampaignMain.js
@@ -11,7 +11,21 @@ async function init() {
   });
   await loadTemplate('../../Templates/playersSideBar.html', '#playersSideBar');
   await loadTemplate('../../Templates/textAndNumberField.html', '.miscValueField');
-  await loadTemplate('../../Templates/characteristicBlock.html', '.characteristicBlock');
+  await loadTemplate('../../Templates/characteristicBlock.html', '.characteristic', () =>{
+    characteristicBlock();
+  });
   await loadTemplate('../../Templates/logTiradas.html', '#logTiradas');
 
+}
+
+function characteristicBlock() {
+  const chars = ['STR', 'DEX', 'CONS', 'WIS', 'INT', 'CHA'];
+  const div = document.querySelector('#characteristicsGroup');
+  const characteristics = document.querySelector('#characteristicBlock');
+
+  chars.forEach((char) => {
+    const clon = characteristics.content.cloneNode(true);
+    clon.querySelector('p').textContent = char;
+    div.appendChild(clon);
+  })
 }
