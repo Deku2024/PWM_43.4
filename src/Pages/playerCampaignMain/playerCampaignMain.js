3fda1dce6cdf68e4ab682a97655a8891d183706a
ivanl <ivanlujmor@gmail.com>
2026-03-28 13:36:22 +0000

    Terminado menú de navegación campaña

--- src/Pages/playerCampaignMain/playerCampaignMain.js
+++ src/Pages/playerCampaignMain/playerCampaignMain.js
@@ -1,4 +1,6 @@
 import { loadTemplate } from '../../Scripts/script.js';
+import { initCampaignMenu } from '../../Scripts/auxiliar-campaign-script.js';
+import { initMenu } from '../../Scripts/auxiliar-script.js';
 
 document.addEventListener('DOMContentLoaded', async function() {
   await init();
@@ -7,7 +9,9 @@ document.addEventListener('DOMContentLoaded', async function() {
 async function init() {
 
   await loadTemplate('../../Templates/headerLoggedIn.html', 'header', () => {
-    loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName');
+    loadTemplate('../../Templates/userIconAndName.html', '.userIconAndName', () => {
+      initCampaignMenu();
+    });
   });
   await loadTemplate('../../Templates/playersSideBar.html', '#playersSideBar');
   await loadTemplate('../../Templates/textAndNumberField.html', '.miscValueField');
