7138a9133a5a29541bf6adf0bc272226decb8c74
Carla <cargom2205@gmiail.com>
2026-04-11 19:10:07 +0100

    logIn migrado y se han añadido componentes Angular de otras páginas

--- dnd/src/app/app.routes.ts
+++ dnd/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
-  {path: 'home',
-    loadComponent: () => import('./pages/home/home').then(m => m.Home)
+  { path: 'home', loadComponent: () => import('./pages/home/home').then((m) => m.Home) },
+  { path: 'logIn', loadComponent: () => import('./pages/log-in/log-in').then((m) => m.LogIn) },
+  { path: 'signIn', loadComponent: () => import('./pages/sign-in/sign-in').then((m) => m.SignIn) },
+  {
+    path: 'profileSettings',
+    loadComponent: () =>
+      import('./pages/profile-settings/profile-settings').then((m) => m.ProfileSettings),
   },
-
-  { path: '**', redirectTo: 'home' }
+  { path: 'joinSession', loadComponent: () => import('./pages/join-session/join-session').then((m) => m.JoinSession) },
+  { path: '**', redirectTo: 'home' },
 ];
---
+++ dnd/src/app/pages/join-session/join-session.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-session',
+  imports: [],
+  templateUrl: './join-session.html',
+  styleUrl: './join-session.css',
+})
+export class JoinSession {}
