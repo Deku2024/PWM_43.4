9d2e484ba3c5ea7c7f7a14803db9454de1a77b75
Carla <cargom2205@gmiail.com>
2026-04-17 16:22:30 +0100

    Añadido firebase al proyecto.

---
+++ dnd/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCv4k9jwqiLLzmAR-cMREo9_xrxDpVzPPI",
+    authDomain: "pwm-43-4.firebaseapp.com",
+    projectId: "pwm-43-4",
+    storageBucket: "pwm-43-4.firebasestorage.app",
+    messagingSenderId: "123702675910",
+    appId: "1:123702675910:web:b766514b2ff8eb0f078ac7",
+    measurementId: "G-23D9B6FB0D"
+  }
+}
