6e4029a578393a402ec2b2bbe7c65d969f140fac
Hirahi Torrejón Cruz <hirahi2024@gmail.com>
2026-05-03 19:56:34 +0100

    Añadido log in

--- dnd/src/app/components/user-icon-and-name-component/user-icon-and-name-component.ts
+++ dnd/src/app/components/user-icon-and-name-component/user-icon-and-name-component.ts
@@ -1,16 +1,22 @@
-import { Component, input, InputSignal, output } from '@angular/core';
+import { Component, input, InputSignal, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'user-icon-and-name-component',
-  imports: [],
+  standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './user-icon-and-name-component.html',
   styleUrl: './user-icon-and-name-component.css',
 })
 export class UserIconAndNameComponent {
   userProfile: InputSignal<boolean> = input<boolean>(false);
 
-  constructor(private router: Router) {}
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  public user$ = this.authService.currentUser$;
 
   goToProfileSettings(): void {
     this.router.navigate(['/profileSettings']);
--- dnd/src/app/services/auth.service.ts
+++ dnd/src/app/services/auth.service.ts
@@ -4,33 +4,29 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   updateProfile,
+  authState, // <-- 1. Importamos authState
 } from '@angular/fire/auth';
-import { Firestore, doc, setDoc } from '@angular/fire/firestore'; // Importar Firestore
+import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private auth = inject(Auth);
-  private firestore = inject(Firestore); // Inyectar el servicio de base de datos
+  private firestore = inject(Firestore);
+  public currentUser$ = authState(this.auth);
 
   login(email: string, password: string) {
     return signInWithEmailAndPassword(this.auth, email, password);
   }
 
   async register(email: string, password: string, username: string) {
-    // 1. Crear el usuario en Authentication
     const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
     const user = userCredential.user;
 
-    // 2. Actualizar el perfil básico (opcional, pero recomendado)
     await updateProfile(user, { displayName: username });
-
-    // 3. CREAR LA COLECCIÓN Y EL DOCUMENTO EN FIRESTORE
-    // doc(base_de_datos, 'nombre_coleccion/ID_del_documento')
     const userDocRef = doc(this.firestore, `users/${user.uid}`);
 
-    // Guardamos los datos que queremos ver en la consola
     await setDoc(userDocRef, {
       uid: user.uid,
       username: username,
